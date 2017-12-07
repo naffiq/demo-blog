79655237f7efce061f19fd5b03b94d63957f30e8
Galymzhan Abdugalimov <abdu.galymzhan@gmail.com>
2017-12-07 12:37:27 +0600

    Fix #1 — Config for console completed

--- config/mode_prod.php
+++ config/mode_prod.php
@@ -29,5 +29,20 @@ return [
     ],
     'console' => [
 
+        'components' => [
+
+            'db' => [
+                'class' => 'yii\db\Connection',
+                'dsn' => 'mysql:host=localhost;dbname=db_new_banners',
+                'username' => 'db_new_banners',
+                'password' => 'dMQ6D4a7rsEiahzB',
+                'charset' => 'utf8mb4',
+
+                // Schema cache options (for production environment)
+                //'enableSchemaCache' => true,
+                //'schemaCacheDuration' => 60,
+                //'schemaCache' => 'cache',
+            ]
+        ]
     ]
 ];
